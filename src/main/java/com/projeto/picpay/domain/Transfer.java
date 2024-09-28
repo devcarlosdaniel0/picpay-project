542b7ce1ae7f290386459d61a387f30639485bfa
Carlos <cadanielmarchesin@gmail.com>
2024-09-28 08:52:40 -0300

    criação da Transfer

---
+++ src/main/java/com/projeto/picpay/domain/Transfer.java
@@ -0,0 +1,31 @@
+package com.projeto.picpay.domain;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.util.UUID;
+
+@Entity
+@Table(name = "tb_transfer")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Transfer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @ManyToOne
+    @JoinColumn(name = "wallet_sender_id")
+    private Wallet sender;
+
+    @ManyToOne
+    @JoinColumn(name = "wallet_reciver_id")
+    private Wallet reciver;
+
+    private BigDecimal value;
+}
